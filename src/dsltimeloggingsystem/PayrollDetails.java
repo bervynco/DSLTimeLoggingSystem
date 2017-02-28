a105969fa632dba9626662741472f9b3727722d6
Bervyn Co <bervyn_co2010@yahoo.com>
2017-02-28 11:52:52 +0800

    PayrollDetails class

---
+++ src/dsltimeloggingsystem/PayrollDetails.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsltimeloggingsystem;
+
+/**
+ *
+ * @author L R E
+ */
+public class PayrollDetails {
+    private float rate;
+    private float sssDeduction;
+    private float pagibigDeduction;
+    private float philHealthDeduction;
+    private float bonus;
+    private float cashAdvance;
+    private float loan;
+    private float days;
+    private float overTime;
+    private float totalSalary;
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public float getSssDeduction() {
+        return sssDeduction;
+    }
+
+    public void setSssDeduction(float sssDeduction) {
+        this.sssDeduction = sssDeduction;
+    }
+
+    public float getPagibigDeduction() {
+        return pagibigDeduction;
+    }
+
+    public void setPagibigDeduction(float pagibigDeduction) {
+        this.pagibigDeduction = pagibigDeduction;
+    }
+
+    public float getPhilHealthDeduction() {
+        return philHealthDeduction;
+    }
+
+    public void setPhilHealthDeduction(float philHealthDeduction) {
+        this.philHealthDeduction = philHealthDeduction;
+    }
+
+    public float getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(float bonus) {
+        this.bonus = bonus;
+    }
+
+    public float getCashAdvance() {
+        return cashAdvance;
+    }
+
+    public void setCashAdvance(float cashAdvance) {
+        this.cashAdvance = cashAdvance;
+    }
+
+    public float getLoan() {
+        return loan;
+    }
+
+    public void setLoan(float loan) {
+        this.loan = loan;
+    }
+
+    public float getDays() {
+        return days;
+    }
+
+    public void setDays(float days) {
+        this.days = days;
+    }
+
+    public float getOverTime() {
+        return overTime;
+    }
+
+    public void setOverTime(float overTime) {
+        this.overTime = overTime;
+    }
+
+    public float getTotalSalary() {
+        return totalSalary;
+    }
+
+    public void setTotalSalary(float totalSalary) {
+        this.totalSalary = totalSalary;
+    }
+    
+    
+}
